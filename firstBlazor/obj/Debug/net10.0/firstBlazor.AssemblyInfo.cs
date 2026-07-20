be7238bc68dcfb43cf6376e2a385673b9b8f6e2e
Fiijoey <kofimensah3730@gmail.com>
2026-07-20 20:41:34 +0000

    Refactor checkout page to use OrderReview and AddressEditor components for improved order review and address input

--- firstBlazor/obj/Debug/net10.0/firstBlazor.AssemblyInfo.cs
+++ firstBlazor/obj/Debug/net10.0/firstBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("firstBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46ff7729089e8258d3439c313223ca394444ffb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+941ca4a066081fe7e2c196f2ffc2ffb59aec4ee4")]
 [assembly: System.Reflection.AssemblyProductAttribute("firstBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("firstBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
